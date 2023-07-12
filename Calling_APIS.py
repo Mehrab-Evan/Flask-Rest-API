ebe38a3a40d09b6e7e5b90464285623e42587fc7
MD. Mehrab Evan <qubee.evan15@gmail.com>
2023-07-12 12:58:25 +0600

    FROM app2.py I have created some routes and this route is using render_template and accessing html files and GET and POST method also used in it

--- Calling_APIS.py
+++ Calling_APIS.py
@@ -10,7 +10,7 @@
 # This Calling_API file is almost working as a postman api testing
 import requests
 
-Server = "http://127.0.0.1:5000/"
+Server = "http://localhost:5000/"
 
 payload = {
     "topic": "Research and Development",
@@ -18,7 +18,21 @@ payload = {
     "problem": "Some problem description"
 }
 
-response = requests.put(Server + "task/1", json=payload)
+payload2 = [{"topic": "Research and Development", "link": "https://example.com", "problem": "Some problem description"},
+            {"topic": "Flask - Rest API", "link": "https://example2.com", "problem": "Some problem description"},
+            {"topic": "Langchain", "link": "https://example3.com", "problem": "Some problem description"}
+]
+
+
+for i in range(len(payload2)):
+    response = requests.put(Server + "task/" + str(i), payload2[i])
+    print(response.json())
+
+input()
+# response = requests.put(Server + "task/1", json=payload)
 response2 = requests.get(Server + "task/1")
 print(response2.json())
-print(response.json())
+# print(response.json())
+
+
+
--- apifile_for_2nd_task.py
+++ apifile_for_2nd_task.py
@@ -1,8 +1,25 @@
 from flask import Flask, request
-from flask_restful import Resource, Api, reqparse
+from flask_restful import Resource, Api, reqparse, fields, marshal_with
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 api = Api(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Medatabase.db'
+db = SQLAlchemy(app)
+
+class TopicModel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    topic = db.Column(db.String(200), nullable=False)
+    link = db.Column(db.String(200), nullable=False)
+    problem = db.Column(db.String(200), nullable=False)
+
+    def __repr__(self):
+        return f"Topic(topic = {self.topic}, links = {self.link}, problem = {self.problem})"
+
+# ekbar run kore felar poree ar run er dorkar nai nahole barbar create hbe
+# with app.app_context():
+#     db.create_all()
+
 
 
 task_put_args = reqparse.RequestParser()
@@ -11,21 +28,36 @@ task_put_args.add_argument("link", type=str, help="ERROR MESSAGE")
 task_put_args.add_argument("problem", type=str, help="ERROR MESSAGE")
 
 # videos dictionary will store the infos
-videos = {}
 
 Tasks = {
     1: {"topic": "FLASK-REST", "link":"www.youtube.com", "problem":"Still did not work with Database"},
     2: {"topic": "Langchain", "link":"www.youtube.com", "problem":"Did not complete it"},
 }
+
+resource_fields = {
+    'id':fields.Integer,
+    'topic': fields.String,
+    'link': fields.String,
+    'problem': fields.String
+}
+# The marshal_with in 45 line will take  the syntax of 37 line resource field to get the datas from get
 class Task_Learned(Resource):
+    @marshal_with(resource_fields)
     def get(self, task_id):
         # Will send the specific video I ask for
-        return Tasks[task_id]
+        result = TopicModel.query.filter_by(id = task_id).first()
+        return result
     # Creating New Task using put method
+    @marshal_with(resource_fields)
     def put(self, task_id):
         # print(request.form)
         args = task_put_args.parse_args()
-        return {task_id: args}
+        tasks = TopicModel(id=task_id, name=args['topic'], link=args['link'], problem=args['problem'])
+        # These are like git
+        db.session.add(tasks)
+        db.session.commit()
+        return tasks, 201
+
 
 # Asking for the specific video through this api
 api.add_resource(Task_Learned, "/task/<int:task_id>")
